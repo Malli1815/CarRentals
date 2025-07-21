fe24468096231c8d850b5ef6c43411a0bdc7bdb1
VIRAT <venkatamallikharjunaraobhaggi@gmail.com>
2025-07-21 21:13:07 +0530

    added files

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://car-rentals-c6a8.vercel.app',
   credentials: true
 }));
 
